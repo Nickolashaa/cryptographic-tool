23c655c3b4effb59c6e304f556848ff7afd533e7
Nickolashaa <gracevnikolaj220@gmail.com>
2025-08-12 03:04:50 +0300

    include code router

--- bot/router.py
+++ bot/router.py
@@ -1,8 +1,9 @@
 from aiogram import Router
-from .handlers import start_router
+from .handlers import start_router, code_router
 
 
 def get_routers() -> Router:
     router = Router()
     router.include_router(start_router)
+    router.include_router(code_router)
     return router
